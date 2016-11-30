102651d76bb2959ebc7976179f81b2b6ebd3370e
gwankyun <kyunsiu@gmail.com>
2016-11-30 23:10:13 +0800

    初始化代碼

---
+++ Program.fs
@@ -0,0 +1,10 @@
+﻿// 在 http://fsharp.org 上了解有关 F# 的详细信息
+// 请参阅“F# 教程”项目以获取更多帮助。
+open FsCheck
+open Extension
+
+[<EntryPoint>]
+let main argv = 
+    Check.Verbose (fun (x: char) -> 'a' |> Char.isLeft = true)
+    printfn "%A" argv
+    0 // 返回整数退出代码
