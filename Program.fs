5501c92971807fab69083cc4484f1513eade1263
gwankyun <kyunsiu@gmail.com>
2016-12-04 12:08:22 +0800

    增加Char和Tuple模塊的測試

--- Program.fs
+++ Program.fs
@@ -2,9 +2,17 @@
 // 请参阅“F# 教程”项目以获取更多帮助。
 open FsCheck
 open Extension
+open System
+
+type ListProperties =
+    static member ``Char.isLeft``(x: char) = 'a' |> Char.isLeft
+    static member ``Char.isRight``(x: char) = 'j' |> Char.isRight
+    static member ``Tuple.first``(x: int) = x = ((x, 1) |> Tuple.first)
+    static member ``Tuple.second``(x: int) = x = ((x, 1) |> Tuple.first)
+    
 
 [<EntryPoint>]
-let main argv = 
-    Check.Verbose (fun (x: char) -> 'a' |> Char.isLeft = true)
+let main argv =
+    Check.QuickAll<ListProperties>()
     printfn "%A" argv
     0 // 返回整数退出代码
